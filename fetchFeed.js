c43078657e73a0813f0549f99ccac1d29a5898f5
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-07-31 19:26:03 +0100

    Delete fetchFeed.js

--- fetchFeed.js
+++
@@ -1,7 +0,0 @@
-import Parser from 'rss-parser';
-const parser = new Parser();
-
-export default async function fetchFeedSummaries(url) {
-  const feed = await parser.parseURL(url);
-  return feed.items.map(item => item.contentSnippet || item.summary || item.description || '');
-}
