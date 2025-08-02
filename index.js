718d2ec45f02a976e76e8ba3d5240b764da190e0
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-08-02 17:45:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import composeRouter from "./routes/compose.js";
+
 dotenv.config();
 
 const app = express();
+
+// parsers
 app.use(cors());
 app.use(express.json({ limit: "6mb" }));
 app.use(express.urlencoded({ extended: true, limit: "6mb" }));
-app.use(express.text({ type: "*/*", limit: "6mb" }));
+app.use(express.text({ type: ["text/*", "application/xml"], limit: "6mb" }));
 
 app.get("/healthz", (_req, res) => res.send("ok"));
+
+// mount
 app.use("/compose", composeRouter);
 
 const port = process.env.PORT || 3000;
